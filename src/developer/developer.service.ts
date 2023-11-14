de6620d9e3a7821d0f1174893cd2931d0c5f5e1a
camposAaron <campos.aaron.15@gmail.com>
2023-11-13 22:07:51 -0600

    update dto

--- src/developer/developer.service.ts
+++ src/developer/developer.service.ts
@@ -178,7 +178,7 @@ export class DeveloperService {
       })),
     });
 
-    return
+    return;
   }
 
   remove(id: number) {
--- src/developer/dto/update-developer.dto.ts
+++ src/developer/dto/update-developer.dto.ts
@@ -1,6 +1,4 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateDeveloperDto } from './create-developer-dto';
 
-export class UpdateDeveloperDto extends PartialType(CreateDeveloperDto) {
-    cvUrl: string;
-}
+export class UpdateDeveloperDto extends PartialType(CreateDeveloperDto) {}
